a0b4cc093c99bdec2226f489f7d7f37d7c94f27a
rosaLivia <127208803+rosaLivia@users.noreply.github.com>
2024-05-23 10:18:36 -0400

    docs: Adicionando layout cadastro e configuração do banco de dados para instânciação

---
+++ trab/app/src/main/java/com/example/discorverplaces/View/Cadastro.java
@@ -0,0 +1,42 @@
+package com.example.discorverplaces.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.discorverplaces.Entity.User;
+import com.example.discorverplaces.R;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+    public void salvar(View view){
+        EditText editxtNome   = findViewById(R.id.editxtNome);
+        EditText editxtEmail  = findViewById(R.id.editxtEmail);
+        EditText editxtSenha  = findViewById(R.id.editxtSenha;
+        Button   bntSalvar    = findViewById(R.id.bntSalvar);
+
+
+        User Usario =  new User();
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
