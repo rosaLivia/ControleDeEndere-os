7b9052f21926e4add90326c6bf84779447e9fe0a
WagnerRdeV <engsoftwagner242@gmail.com>
2024-05-22 22:02:22 -0300

    feat: Alterando a classe SeedDAO

--- trab/app/src/main/java/com/example/discorverplaces/DAO/SeedDAO.java
+++ trab/app/src/main/java/com/example/discorverplaces/DAO/SeedDAO.java
@@ -4,20 +4,32 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.discorverplaces.Entity.Seed;
 
+import java.util.List;
 
 @Dao
-public enum SeedDAO {
+public interface SeedDAO {
+    @Query("SELECT * FROM seed")
+    List<Seed> getAll();
 
-    @Query("SELECT * FROM SE")
-    List<Sedd> getAll();
+    @Query("SELECT * FROM seed WHERE enderecoID = :id")
+    Seed getById(int id);
+
     @Insert
-    void insert(Seed seed);
+    void insertll(Seed seed);
+
+    @Update
+    void update(Seed seed);
 
     @Delete
     void delete(Seed seed);
 
+    @Query("DELETE FROM seed WHERE enderecoID = :id")
+    void deleteById(int id);
 
+    @Query("DELETE FROM seed")
+    void deleteAll();
 }
