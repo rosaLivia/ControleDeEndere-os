a5393d772cf3c73a93fe478282709d3c3e33edf5
WagnerRdeV <engsoftwagner242@gmail.com>
2024-05-23 03:32:46 -0300

    docs: Adicionando as tela inicial e login

--- trab/app/src/main/java/com/example/discorverplaces/View/Login.java
+++ trab/app/src/main/java/com/example/discorverplaces/View/Login.java
@@ -1,6 +1,9 @@
 package com.example.discorverplaces.View;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.discorverplaces.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnGoToMain = findViewById(R.id.btnGoToMain);
+        btnGoToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciando a atividade MainActivity
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
